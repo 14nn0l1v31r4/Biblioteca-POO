6b8c37427c8ac00d5fb2dfc3950de2e92a9e4312
Iann Molendolff <iannmolendolff@gmail.com>
2024-09-15 17:20:47 -0300

    Navbar

---
+++ Components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className=" flex text-black font-bold">
+      teste
+    </footer>
+  )
+}
+
+export default Footer
---
+++ Components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <header className="font-bold text-black p-3">
+        Bem Vindo
+    </header>
+  )
+}
+
+export default Hero
